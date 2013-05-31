181eb4203dcdbf06e97ef481eb2a2a022c0077af
Horace Li <horaceli@gmail.com>
2013-05-31 17:31:03 +0100

    Tests

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -3,10 +3,10 @@ __author__ = 'Horace'
 import unittest
 from code import index
 
-class MyTest(unittest.TestCase):
-    def test(self):
+class TestNumbers(unittest.TestCase):
+    def test_one(self):
         codeTest = index()
         self.assertEqual(codeTest.func(3), 4)
 
-def test_answer():
-    assert index.func(3) == 5
+def test_two():
+    assert index.func(5) == 6 
