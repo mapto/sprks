e8b09ed22bfe19c355b3dc315f0e831ac43f0c0d
Horace Li <horaceli@gmail.com>
2013-05-31 16:17:46 +0100

    Update code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+import web
+import json
+
+urls = (
+    '/', 'index'
+)
+
+
+class index:
+    def GET(self):
+        db = web.database(dbn='mysql', user='user', pw='password', db='test')
+        table = db.select('pw_policy')
+        return json.dumps(table[0]) + " hello world2"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+	
