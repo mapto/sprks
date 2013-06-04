48e727b7e8e45cb383c89b647e9dfc58a1dc00ab
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2013-06-04 15:51:30 +0100

    Writing back to template

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 __author__ = 'zcabh_000'
 from web import form
 import web
+import json
 render = web.template.render('C:/Users/zcabh_000/PycharmProjects/sprks/templates/')
 
 
@@ -20,9 +21,9 @@ class login:
         db = web.database(dbn='mysql', user='root', pw='12345', db='sprks')
         id = db.insert('pw_policy', plen=i.plen, psets=i.psets, pdict=i.pdict, psim=i.psim, prenew=i.prenew,
                        pattempts=i.pattempts, precovery=i.precovery)
-        if id > 0:
-            return render.test()
-
+        id_tmp = db.select('pw_policy', where="id=$id", vars=locals())[0]
+        return render.test(id_tmp.id, id_tmp.plen, id_tmp.psets, id_tmp.pdict, id_tmp.psim, id_tmp.prenew,
+                           id_tmp.pattempts, id_tmp.precovery)
 
 
 
