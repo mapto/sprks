95c9beec7ec91cf81981afc28587384e29774b0e
Horace Li <horaceli@gmail.com>
2013-06-04 14:48:03 +0100

    Migrating to PyTest

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sprks',
+    version='',
+    packages=['sim', 'test'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
--- test/simulation/test_mainsim.py
+++ test/simulation/test_mainsim.py
@@ -1,19 +1,36 @@
 __author__ = 'Horace'
 
-import unittest
+
 from sim.simulation import *
 
 
-class TestSim(unittest.TestCase):
+class TestMaxSec:
 
     def test_calc_risk_prob(self):
         policy = simulation()
         policy.set_policy('plen', 8)
         policy.set_policy('psets', 3)
-        self.assertEqual(policy.calc_risk_prob(),0.375)
+        assert policy.calc_risk_prob() == 0.375
+
+    def test_calc_risk_impact(self):
+        policy = simulation()
+        assert policy.calc_risk_impact() == 1
 
     def test_calc_prod_cost(self):
         policy = simulation()
         policy.set_policy('plen', 8)
         policy.set_policy('psets', 3)
-        self.assertEqual(policy.calc_prod_cost(),7)
+        assert policy.calc_prod_cost() == 7
+
+
+class TestMinSec:
+
+    def test_calc_risk_prob(self):
+        policy = simulation()
+
+    def test_calc_risk_impact(self):
+        policy = simulation()
+        assert policy.calc_risk_impact() == 1
+
+    def test_calc_prod_cost(self):
+        policy = simulation()
