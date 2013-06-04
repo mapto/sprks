78ca016ba31a13a52a4529484bbb19a25e26679d
Horace Li <horaceli@gmail.com>
2013-06-04 12:37:41 +0100

    Added basic class outlines for pdict, phist

--- sim/policy_pdict.py
+++ sim/policy_pdict.py
@@ -2,34 +2,29 @@ import policy_interface
 
 __author__ = 'Horace'
 
-
-class policy_plen(policy_interface.policy_interface):
+class policy_pdict(policy_interface.policy_interface):
     def __init__(self):
         self.__init__(3)
 
     def __init__(self, value):
-        if value in [0, 6, 8, 10, 12]:
+        if type(value) == bool:
             self.param = value
         else:
             raise Exception('Wrong parameter type')
+
     def get_risk_prob(self):
 
-        return {
-            0: 1,
-            6: .9,
-            8: .75,
-            10: .6,
-            12: .4,
-        }[self.param]
+        if self.param:
+            return .4
+        else:
+            return .6
 
     def get_risk_impact(self):
         return 1 # need a value that does not affect the formula
 
     def get_prod_cost(self):
-        return {
-            0: 1,
-            6: 4,
-            8: 5,
-            10: 6,
-            12: 7,
-        }[self.param]
+
+        if self.param:
+            return 2
+        else:
+            return 1
--- sim/policy_phist.py
+++ sim/policy_phist.py
@@ -2,24 +2,23 @@ import policy_interface
 
 __author__ = 'Horace'
 
-
-class policy_plen(policy_interface.policy_interface):
+class policy_psets(policy_interface.policy_interface):
     def __init__(self):
         self.__init__(3)
 
     def __init__(self, value):
-        if value in [0, 6, 8, 10, 12]:
+        if value in [0, 1, 2, 3]:
             self.param = value
         else:
             raise Exception('Wrong parameter type')
+
     def get_risk_prob(self):
 
         return {
-            0: 1,
-            6: .9,
-            8: .75,
-            10: .6,
-            12: .4,
+            0: .8,
+            1: .6,
+            2: .4,
+            3: .3,
         }[self.param]
 
     def get_risk_impact(self):
@@ -27,9 +26,8 @@ class policy_plen(policy_interface.policy_interface):
 
     def get_prod_cost(self):
         return {
-            0: 1,
-            6: 4,
-            8: 5,
-            10: 6,
-            12: 7,
+            0: 0,
+            1: 3,
+            2: 5,
+            3: 10,
         }[self.param]
