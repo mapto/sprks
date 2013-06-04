5947f7ebe3d6b1aea9fd9da337d5280ff8de13a4
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2013-06-04 12:49:14 +0100

    Added form template

--- login.py
+++ login.py
@@ -1,24 +1,27 @@
 __author__ = 'zcabh_000'
 from web import form
 import web
+render = web.template.render('C:/Users/zcabh_000/PycharmProjects/sprks/templates/')
 
 
 class login:
     def GET(self):
-        login = form.Form(
-            form.Textbox('username'),
-            form.Password('password'),
-            form.Button('Login'),
-            form.Radio('radio', ['Radio', 'R']), )
-        f = login()
-        return f.render()
+        #login = form.Form(
+         #   form.Textbox('username'),
+          #  form.Password('password'),
+           # form.Button('Login'),
+            #form.Radio('radio', ['Radio', 'R']), )
+       # f = login()
+        #return f.render()
+        return render.login()
 
     def POST(self):
         i = web.input()
         db = web.database(dbn='mysql', user='root', pw='12345', db='sprks')
-        id = db.insert('pw_policy', plen=3, psets=3, pdict=1, psim='test', prenew='monthly', pattempts=2, precovery='manned')
+        id = db.insert('pw_policy', plen=i.plen, psets=i.psets, pdict=i.pdict, psim=i.psim, prenew=i.prenew,
+                       pattempts=i.pattempts, precovery=i.precovery)
         if id > 0:
-            return 'OK'
+            return render.test()
 
 
 
