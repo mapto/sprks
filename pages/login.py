d78032ebffec0fbf6e6c2df884bbc233ca4f25df
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2013-06-11 16:44:49 +0100

    Fixed setting issues

--- pages/login.py
+++ pages/login.py
@@ -16,20 +16,20 @@ class login:
     def GET(self):
         session.mysession.session.loggedin=False
         session.mysession.session.user='Anonimous'
-        return render.login()
+        return self.render.login()
 
     def POST(self):
         i = web.input()
         usrname = i.username
         password = hashlib.sha224(i.password).hexdigest()
-        id_tmp = db.select('users', where="username=$usrname&&password=$password", vars=locals())
+        id_tmp = self.db.select('users', where="username=$usrname&&password=$password", vars=locals())
         if len(id_tmp) > 0:
             session.mysession.session.loggedin=True
             session.mysession.session.user=usrname
             session.mysession.session.id=id_tmp[0].Id
             raise web.seeother('/pwpolicy')
         else:
-            return render.login()
+            return self.render.login()
 
 
 
