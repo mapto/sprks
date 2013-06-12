ed04ecd752c71366edad5216477a814d285af399
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2013-06-12 09:54:55 +0100

    fixes #15

--- pages/register.py
+++ pages/register.py
@@ -2,6 +2,7 @@ __author__ = 'zcabh_000'
 import web
 import hashlib
 from settings import settings
+import session
 
 
 class register:
@@ -22,4 +23,11 @@ class register:
         if len(tmp) > 0:
             return self.render.register("User already exists")
         id = self.db.insert('users', username=i.username, email=i.email, password=hashlib.sha224(i.password).hexdigest())
-        raise web.seeother('/login')
+        id_tmp = self.db.select('users', where="username=$usrname", vars=locals())
+        if len(id_tmp) > 0:
+            session.mysession.session.loggedin=True
+            session.mysession.session.user=usrname
+            session.mysession.session.id=id_tmp[0].Id
+            raise web.seeother('/pwpolicy')
+        else:
+            return self.render.login()
