e8da4c37d5c4b6a2ed2cfcb16f1c6b78c772c68e
Zhanelya Subebayeva <subebo91@inbox.ru>
2013-06-12 16:55:07 +0100

    returned dummyclass

---
+++ dummy_class.py
@@ -0,0 +1,32 @@
+import web
+import session
+
+import controllers.register
+import controllers.secured_page
+import controllers.secured_page2
+import controllers.login
+import controllers.pwrecovery
+import controllers.pwrequest
+import controllers.code_get_post
+import controllers.score
+
+urls = ('/', controllers.login.login,
+        '/login', controllers.login.login,
+        '/register', controllers.register.register,
+        '/secured_page', controllers.secured_page.secured_page,
+        '/secured_page2', controllers.secured_page2.secured_page,
+        '/pwrecovery/(.*)', controllers.pwrecovery.pwrecovery,
+        '/pwrecovery', controllers.pwrecovery.pwrecovery,
+        '/pwrequest', controllers.pwrequest.pwrequest,
+        '/score', controllers.score.score,
+        '/pwpolicy', controllers.code_get_post.pwpolicy_form # this URL is also being used in views/index.html for AJAX services
+)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    if web.config.get('_session') is None:
+        session.mysession.session = web.session.Session(app, session.mysession.store, initializer={'user': 'anonymous', 'loggedin': False, 'id': 0})
+        web.config._session = session
+    else:
+        session.mysession.session = web.config._session
+    app.run()
