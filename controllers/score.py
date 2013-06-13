8763cecd0087fed5e7d177f9be122f201c8415a2
Zhanelya Subebayeva <subebo91@inbox.ru>
2013-06-13 13:46:43 +0100

    Updated score.py, score.html,index.html

--- controllers/score.py
+++ controllers/score.py
@@ -1,8 +1,20 @@
 __author__ = 'zhanelya'
 
 import web
+import session
 from environment import render_private as render
+from environment import db
 
 class score:
     def GET(self):
-        return render.score(8,2,3)
+        if session.mysession.session.loggedin:
+            #use this variable to request any ID number
+            id_user = session.mysession.session.id
+            check = db.select('scores', vars=locals())
+            if len(check) > 0:
+                result_get = check[0]
+                return render.score(8,2,3,result_get.prob, result_get.impact, result_get.cost)
+            else:
+                return 'not found'
+        else:
+            raise web.seeother('/login')
