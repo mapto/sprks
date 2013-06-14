dc6ec1efc6d2da27b4ffb781c2e8904967019657
Zhanelya Subebayeva <subebo91@inbox.ru>
2013-06-14 15:03:37 +0100

    Updated score.py, score.html

--- controllers/score.py
+++ controllers/score.py
@@ -12,17 +12,46 @@ class score:
         if session.mysession.session.loggedin:
             #use this variable to request any ID number
             id_user = session.mysession.session.id
-            your_risk = db.select('scores', where="userid=$id_user and score_type=1", vars=locals())
 
+            your_risk = db.select('scores', where="userid=$id_user and score_type=1", vars=locals())
             your_pc = db.select('scores', where="userid=$id_user and score_type=2", vars=locals())
-            #closest_risk =
-            #closest_pc =
+
             if len(your_risk) > 0 and len(your_pc) > 0:
-                result_y_risk = your_risk[0]
-                result_y_pc = your_pc[0]
-                return render.score(result_y_risk.score_value, result_y_risk.date, result_y_risk.rank,
-                                    result_y_pc.score_value, result_y_pc.date, result_y_pc.rank)
+                your_risk = your_risk[0]
+                your_pc = your_pc[0]
+
+                c_risk = your_risk.score_value
+                c_risk_when = your_risk.date
+                c_risk_rank = your_risk.rank
+                c_pc = your_pc.score_value
+                c_pc_when = your_pc.date
+                c_pc_rank = your_pc.rank
+
+                rank = your_risk.rank
+
+                closest_risk = db.select('scores', where="score_type=1 and rank=$rank-1", vars=locals())
+                if len(closest_risk) > 0:
+                    closest_risk=closest_risk[0]
+
+                    #update closest values if a closest RISK competitor is found
+                    c_risk = closest_risk.score_value
+                    c_risk_when = closest_risk.date
+                    c_risk_rank = closest_risk.rank
+
+                closest_pc = db.select('scores', where="score_type=2 and rank=$rank-1", vars=locals())
+                if len(closest_pc) > 0:
+                    closest_pc=closest_pc[0]
+
+                    #update closest values if a closest ProdCOST competitor is found
+                    c_pc = closest_pc.score_value
+                    c_pc_when = closest_pc.date
+                    c_pc_rank = closest_pc.rank
+
+                return render.score(your_risk.score_value, your_risk.date, your_risk.rank,
+                                    your_pc.score_value, your_pc.date, your_pc.rank,
+                                    c_risk, c_risk_when, c_risk_rank,
+                                    c_pc, c_pc_when, c_pc_rank)
             else:
-                return 'not found'
+                return 'You have not finished any term yet'
         else:
             raise web.seeother('/login')
