95f5d9fe7f0afa04721614e7e433cb6e1cd0bb1f
Zhanelya Subebayeva <subebo91@inbox.ru>
2013-06-14 15:33:12 +0100

    Updated score.py, removed duplicate methods

--- controllers/score.py
+++ controllers/score.py
@@ -5,9 +5,25 @@ import session
 from environment import render_private as render
 from environment import db
 
-
-
 class score:
+    def CHECK_CLOSEST_COMPETITOR(self, your_score):
+        c = your_score.score_value
+        c_when = your_score.date
+        c_rank = your_score.rank
+
+        rank=your_score.rank
+        score_type = your_score.score_type
+
+        closest = db.select('scores', where="score_type=$score_type and rank=$rank-1", vars=locals())
+        if len(closest) > 0:
+                    closest = closest[0]
+
+                    #update closest values if a closest competitor for this score type is found
+                    c = closest.score_value
+                    c_when = closest.date
+                    c_rank = closest.rank
+        return c, c_when, c_rank
+
     def GET(self):
         if session.mysession.session.loggedin:
             #use this variable to request any ID number
@@ -20,37 +36,16 @@ class score:
                 your_risk = your_risk[0]
                 your_pc = your_pc[0]
 
-                c_risk = your_risk.score_value
-                c_risk_when = your_risk.date
-                c_risk_rank = your_risk.rank
-                c_pc = your_pc.score_value
-                c_pc_when = your_pc.date
-                c_pc_rank = your_pc.rank
+                c_risk, c_risk_when, c_risk_rank = self.CHECK_CLOSEST_COMPETITOR(your_risk)
+                c_pc, c_pc_when, c_pc_rank = self.CHECK_CLOSEST_COMPETITOR(your_pc)
 
-                rank = your_risk.rank
-
-                closest_risk = db.select('scores', where="score_type=1 and rank=$rank-1", vars=locals())
-                if len(closest_risk) > 0:
-                    closest_risk=closest_risk[0]
-
-                    #update closest values if a closest RISK competitor is found
-                    c_risk = closest_risk.score_value
-                    c_risk_when = closest_risk.date
-                    c_risk_rank = closest_risk.rank
-
-                closest_pc = db.select('scores', where="score_type=2 and rank=$rank-1", vars=locals())
-                if len(closest_pc) > 0:
-                    closest_pc=closest_pc[0]
-
-                    #update closest values if a closest ProdCOST competitor is found
-                    c_pc = closest_pc.score_value
-                    c_pc_when = closest_pc.date
-                    c_pc_rank = closest_pc.rank
 
                 return render.score(your_risk.score_value, your_risk.date, your_risk.rank,
                                     your_pc.score_value, your_pc.date, your_pc.rank,
                                     c_risk, c_risk_when, c_risk_rank,
                                     c_pc, c_pc_when, c_pc_rank)
+
+
             else:
                 return 'You have not finished any term yet'
         else:
