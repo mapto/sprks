6cc3493d90b3ae70134fef3930dac7687a5b43c5
Zhanelya Subebayeva <subebo91@inbox.ru>
2013-06-14 14:01:54 +0100

    Updated score.py, score.html, styles

--- controllers/score.py
+++ controllers/score.py
@@ -5,15 +5,23 @@ import session
 from environment import render_private as render
 from environment import db
 
+
+
 class score:
     def GET(self):
         if session.mysession.session.loggedin:
             #use this variable to request any ID number
             id_user = session.mysession.session.id
-            check = db.select('scores', vars=locals())
-            if len(check) > 0:
-                result_get = check[0]
-                return render.score(8,2,3,result_get.prob, result_get.impact, result_get.cost)
+            your_risk = db.select('scores', where="userid=$id_user and score_type=1", vars=locals())
+
+            your_pc = db.select('scores', where="userid=$id_user and score_type=2", vars=locals())
+            #closest_risk =
+            #closest_pc =
+            if len(your_risk) > 0 and len(your_pc) > 0:
+                result_y_risk = your_risk[0]
+                result_y_pc = your_pc[0]
+                return render.score(result_y_risk.score_value, result_y_risk.date, result_y_risk.rank,
+                                    result_y_pc.score_value, result_y_pc.date, result_y_pc.rank)
             else:
                 return 'not found'
         else:
