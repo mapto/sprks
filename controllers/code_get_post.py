cdeaab026cb8f2265e1ce80098c90d4759c8e97b
mapto <mapto@ko64eto.com>
2013-06-19 11:47:05 +0100

    * when forwarding time, read previous time and policy from database, not from client * extracted default password policy so it can be reused * when user registers first page is introduction * if first use (not found) notice is shown, it should be welcoming :)

--- controllers/code_get_post.py
+++ controllers/code_get_post.py
@@ -8,6 +8,11 @@ from environment import db
 from models.pw_policy import pw_policy_model
 
 class pwpolicy_form:
+    # the default policy should be specified in a central place and reusable
+    default = {"plen": 8, "psets": 2, "pdict": 0,
+               "phist": 1, "prenew": 1, "pattempts": 0,
+               "pautorecover": 1}
+
     def GET(self):
         if session.mysession.session.loggedin:
             #use this variable to request any ID number
@@ -25,9 +30,15 @@ class pwpolicy_form:
                 notfound=1
                 dt = datetime.now()
                 dtt = dt - timedelta(days=dt.weekday())
-                db.insert('pw_policy', plen=8, psets=2, pdict=0,
-                          phist=1, prenew=1, pattempts=0,
-                          pautorecover=1, userid=id_user, date=dtt.strftime("%Y/%m/%d %H:%M:%S"))
+                # The default policy (i.e. when not specified by user)
+                db.insert('pw_policy', userid=id_user, date=dtt.strftime("%Y/%m/%d %H:%M:%S"),
+                          plen=pwpolicy_form.default["plen"],
+                          psets=pwpolicy_form.default["psets"],
+                          pdict=pwpolicy_form.default["pdict"],
+                          phist=pwpolicy_form.default["phist"],
+                          prenew=pwpolicy_form.default["prenew"],
+                          pattempts=pwpolicy_form.default["pattempts"],
+                          pautorecover=pwpolicy_form.default["pautorecover"])
                 result_get = db.select('pw_policy', where="userid=$id_user", vars=locals())[0]
                 session.mysession.session.date = result_get.date
                 return render.pwpolicy_form(session.mysession.session.user, result_get.userid, result_get.plen, result_get.psets,
@@ -69,28 +80,38 @@ class add:
         web.header('Content-Type', 'application/json')
         usrid = session.mysession.session.id
         sim = simulation()
-        data = json.loads(web.data())
-        dat = eval(data["data"])
-        date = data["date"]
-        dt = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-        dtt = dt + timedelta(days=7)
-        if "pdict" in dat:
-            dict1=1
+#        data = json.loads(web.data())
+#        dat = eval(data["data"])
+#        date = data["date"]
+#        dtt = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+        # get the latest date that the user has submitted a policy and add 7 days to it
+        # if the user hasn't submitted anything, take today's date
+
+        data = pwpolicy_form.default
+        prev_date = datetime.strptime("2013-6-17 9", "%Y-%m-%d %H") # 9am on random Monday
+
+        user_policies = db.select('pw_policy', where="userid=$usrid", order="date DESC", vars=locals())
+        if len(user_policies) > 0:
+            policy = user_policies[0] # get last policy
+            prev_date = datetime.strptime(policy.date, "%Y-%m-%d %H:%M:%S")
+            data = {"plen": policy.plen,
+                    "psets": policy.psets,
+                    "pdict": policy.pdict,
+                    "phist": policy.phist,
+                    "prenew": policy.prenew,
+                    "pattempts": policy.pattempts,
+                    "pautorecover": policy.pautorecover}
         else:
-            dat["pdict"]=0
-        if "pautorecover" in dat:
-            dat["pautorecover"]=1
-        else:
-            dat["pautorecover"]=0
-        if "pattempts" in dat:
-            pattempts1=1
-        else:
-            dat["pattempts"]=0
-        for k, value in dat.iteritems():
+            # update to next Monday after system day as a starting date
+            while prev_date < datetime.now():
+                prev_date = prev_date + timedelta(days=7)
+
+        new_date = prev_date + timedelta(days=7)
+
+        for k, value in data.iteritems():
             sim.set_policy(k, value)
-        db.insert('scores', userid=usrid, score_type=1, score_value = sim.calc_risk_prob(), date=dtt.strftime("%Y/%m/%d %H:%M:%S"), rank=0)
-        db.insert('scores', userid=usrid, score_type=2, score_value = sim.calc_prod_cost(), date=dtt.strftime("%Y/%m/%d %H:%M:%S"), rank=0)
-        db.insert('pw_policy', plen=dat["plen"], psets=dat["psets"], pdict=dat["pdict"],
-                          phist=dat["phist"], prenew=dat["prenew"], pattempts=dat["pattempts"],
-                          pautorecover=dat["pautorecover"], userid=usrid, date=dtt.strftime("%Y/%m/%d %H:%M:%S"))
-        return json.dumps([{"value":dtt.strftime("%Y/%m/%d %H:%M:%S")}])
+
+        db.insert('scores', userid=usrid, score_type=1, score_value = sim.calc_risk_prob(), date=prev_date.strftime("%Y/%m/%d %H:%M:%S"), rank=0)
+        db.insert('scores', userid=usrid, score_type=2, score_value = sim.calc_prod_cost(), date=prev_date.strftime("%Y/%m/%d %H:%M:%S"), rank=0)
+        return json.dumps([{"value": new_date.strftime("%Y/%m/%d %H:%M:%S")}])
