8667cc6ba9d7f4ebc08ee3142442e2bf4adbc65c
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2013-06-21 15:34:03 +0100

    Updated estimator

--- sim/estimator_sklearn_tree.py
+++ sim/estimator_sklearn_tree.py
@@ -12,17 +12,19 @@ class estimator_sklearn_tree(estimator_interface):
         # columns in train_data and test CSV file: plen, psets, pdict, phist, prenew, pattempts, pautorecover
         self.name = "full"
         train_data = genfromtxt('static/data/pw-train-data-' + self.name + '.csv', delimiter=',')
-        #train_data_conv_risk = self.toNormalized_risk(train_data)
-       # train_data_conv_cost = self.toNormalized_cost(train_data)
+        train_data_conv_risk = self.toNormalized_risk(train_data)
+        train_data_conv_cost = self.toNormalized_cost(train_data)
         print "train data"
-        print train_data
+        print train_data_conv_cost
         # columns in train_value CSV file and result: risk_prob, risk_impact, prod_cost
         train_value = genfromtxt('static/data/pw-train-result-' + self.name + '.csv', delimiter=',')
+        print "train value"
+        print train_value
         # test_data = genfromtxt('static/data/pw-test-data.csv', delimiter=',')
-       ## self.risk_prob_model = tree.DecisionTreeRegressor().fit(train_data_conv_risk, train_value[:, 0])
-       # self.risk_impact_model = tree.DecisionTreeRegressor().fit(train_data_conv_risk, train_value[:, 1])
-       # self.prod_cost_model = tree.DecisionTreeRegressor().fit(train_data_conv_cost, train_value[:, 2])
-        self.model = tree.DecisionTreeRegressor().fit(train_data, train_value)
+        #self.risk_prob_model = tree.DecisionTreeRegressor().fit(train_data_conv_risk, train_value[:, 0])
+        #self.risk_impact_model = tree.DecisionTreeRegressor().fit(train_data_conv_risk, train_value[:, 1])
+        self.prod_cost_model = tree.DecisionTreeRegressor().fit(train_data_conv_cost, train_value[:, 2])
+        #self.model = tree.DecisionTreeRegressor().fit(train_data, train_value)
         # print clf.predict(test_data)
         # use this only if you want to explore what the machine learning algorithm learned
         self.exportPDF()
@@ -74,7 +76,6 @@ class estimator_sklearn_tree(estimator_interface):
     # each dimension carries equal weight of 48
     def toNormalized_risk(self, data):
         arr = []
-        print data
         for k in data:
             tmp = []
             complexity = k[0] + k[1] * 3 + k[2] * 12 + k[3] * 4
@@ -83,7 +84,6 @@ class estimator_sklearn_tree(estimator_interface):
             tmp.append(k[5] * 24)
             tmp.append(k[6] * 48)
             arr.append(tmp)
-        print arr
         return arr
 
     # calculate weights of dimensions of cost model
@@ -116,9 +116,10 @@ class estimator_sklearn_tree(estimator_interface):
             # graph = pydot.graph_from_dot_data(dot_data.getvalue())
             # graph.write_pdf("static/data/tree-" + self.name + "-risk-impact.pdf")
             #
-          ##  tree.export_graphviz(self.prod_cost_model, out_file=dot_data)
-           # graph = pydot.graph_from_dot_data(dot_data.getvalue())
-           # graph.write_pdf("static/data/tree-" + self.name + "-prod-cost.pdf")
+            tree.export_graphviz(self.prod_cost_model, out_file=dot_data)
+            graph = pydot.graph_from_dot_data(dot_data.getvalue())
+            graph.write_pdf("static/data/tree-" + self.name + "-prod-cost.pdf")
+
             #
 
         except ImportError:
@@ -140,10 +141,10 @@ if __name__ == "__main__":
     os.chdir("..")
     tool = estimator_sklearn_tree()
     test_data = genfromtxt('static/data/pw-test-data.csv', delimiter=',')
-    #test_data_conv = tool.toNormalized_cost(test_data)
+    test_data_conv = tool.toNormalized_cost(test_data)
     #print test_data_conv
 #    print tool.predict(test_data_conv)
     print "predicted data"
-    print tool.model.predict(test_data)
+    print tool.prod_cost_model.predict(test_data_conv)
 #    tool.risk_impact_model.predict(datapoints)
 #    tool.prod_cost_model.predict(datapoints)
