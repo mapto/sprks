ff6426de979ae01270fb3b06fa13237ba0977ac1
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2013-06-25 19:09:36 +0100

    Updated profile.js. Uses real, not mock data

--- static/js/profile.js
+++ static/js/profile.js
@@ -6,28 +6,36 @@
  * To change this template use File | Settings | File Templates.
  */
 function init() {
- json=json[0];
- var prenew = ( json.prenew);
-            var pattempts = ( json.pattempts);
-            var cost = ( json.cost);
-            var idpolicy = ( json.idpolicy);
-            var psets = ( json.psets);
-            var phist = ( json.phist);
-            var userid = ( json.userid);
-            var pautorecover = ( json.pautorecover);
-            var date = ( json.date);
-            var plen = ( json.plen);
-            var risk = ( json.risk);
-            var pdict = ( json.pdict);
+ json1=json[0];
+ var prenew = ( json1.prenew);
+            var pattempts = ( json1.pattempts);
+            var cost = ( json1.cost);
+            var idpolicy = ( json1.idpolicy);
+            var psets = ( json1.psets);
+            var phist = ( json1.phist);
+            var userid = ( json1.userid);
+            var pautorecover = ( json1.pautorecover);
+            var date = ( json1.date);
+            var plen = ( json1.plen);
+            var risk = ( json1.risk);
+            var pdict = ( json1.pdict);
     $("#risk").text('Risk is '+risk);
     $("#cost").text('Cost is '+cost);
-    createGraph(date,cost,risk);
+    createGraph(date,cost,risk,json);
 }
-function createGraph(date,cost,risk){
+function createGraph(date,cost,risk, data){
+    dps1_1 = [];
+    dps2_1 = [];
+    for(var k in data)
+    {
+        tmpRisk = {label:data[k].date, y:data[k].risk};
+        tmpCost = {label:data[k].date, y:data[k].cost};
+        dps1_1.push(tmpRisk);
+        dps2_1.push(tmpCost);
+    }
 
     var dps1 = [ {label: date, y: risk},{label: date, y: risk + 0.1} ,  {label: date, y: risk-0.2},  {label: date, y: risk},  {label: date, y: risk} ]; //dataPoints – line 1
     var dps2 = [ {label: date, y: cost},{label: date, y: cost + 0.4} ,  {label: date, y: cost+0.1},  {label: date, y: cost+0.6},  {label: date, y: cost} ]; //dataPoints. – line 2
-
  var chart = new CanvasJS.Chart("chartContainer",{
  title :{
  text: "Progress"
@@ -38,12 +46,11 @@ function createGraph(date,cost,risk){
  axisY: {
  title: "Units"
  },
-
  // begin data for 2 line graphs. Note dps1 and dps2 are
  //defined above as a json object. See http://www.w3schools.com/json/
  data: [
- { type: "line", name: "Risk(%) ", showInLegend: true, dataPoints : dps1},
- { type: "line", name: "Productivity cost($ million) ", showInLegend: true, dataPoints : dps2}
+ { type: "line", name: "Risk(%) ", showInLegend: true, dataPoints : dps1_1},
+ { type: "line", name: "Productivity cost($ million) ", showInLegend: true, dataPoints : dps2_1}
  ]
  // end of data for 2 line graphs
 
