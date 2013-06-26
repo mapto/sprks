358b445169b3f8dec8227702022b28281acf977e
mapto <mapto@ko64eto.com>
2013-06-26 14:17:17 +0100

    Incident getter added. Currently a stub, need to add database table

--- models/incident.py
+++ models/incident.py
@@ -1,15 +1,25 @@
 __author__ = 'zcabh_000'
-from sklearn import svm
 
+from environment import db
 
 class incident:
     classes = [range(1, 8)]
 
+    def get_incident(self, cls):
+        list = db.select('incidents', where="idincident=$id", vars=locals())
+        if len(list) == 0:
+            raise KeyError("Incident ID not found.")
+        return list[0]
+
+
     def get_incident_description(self, cls):
-        pass
+        #        return self.get_incident(cls).description
+        return 1
 
     def get_incident_risk(self, cls):
-        pass
+        #        return self.get_incident(cls).risk
+        return 1
 
     def get_incident_cost(self, cls):
-        pass
+        #        return self.get_incident(cls).cost
+        return 1
