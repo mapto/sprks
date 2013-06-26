f99c635e9e239c5b7d757f14367d9d9ba8834973
Zhanelya Subebayeva <subebo91@inbox.ru>
2013-06-26 15:29:15 +0100

    Updated profile page, policy_hostory, added dynamically created table

--- controllers/policy_history.py
+++ controllers/policy_history.py
@@ -2,9 +2,9 @@ __author__ = 'Dan'
 from environment import render_private as render
 import web
 import session
-from models.users import users_model
+from models.policies import policies_model
 import json
-from environment import render_private as render
+
 
 
 class history:
@@ -13,10 +13,11 @@ class history:
             username = session.mysession.session.user
             user_id = session.mysession.session.id
             date = session.mysession.session.date
-            policy_history = users_model().get_policy_history(user_id)
+            policy_history = policies_model().get_policy_history(user_id)
 
             if policy_history is not None:
                 #return json.dumps(policy_history)
+                print json.dumps(policy_history)
                 return render.profile(username, date, policy_history)
             else:
                 return "None"
