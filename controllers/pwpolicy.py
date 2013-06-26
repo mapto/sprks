0e51eba56e85ef3bbdf6f5a693f96b1274b69376
Horace Li <me@horaceli.com>
2013-06-26 16:29:39 +0100

    Minor refactoring

--- controllers/pwpolicy.py
+++ controllers/pwpolicy.py
@@ -8,6 +8,7 @@ from environment import db
 from models.pw_policy import pw_policy_model
 from environment import get_start_time
 
+
 class pwpolicy:
     # the default policy should be specified in a central place and reusable
     default = {"plen": 8, "psets": 2, "pdict": 0,
@@ -15,20 +16,20 @@ class pwpolicy:
                "pautorecover": 1}
 
     def GET(self):
+        """
+        Renders the form to input password policies.
+        """
         if session.mysession.session.loggedin:
             #use this variable to request any ID number
             id_user = session.mysession.session.id
             check = db.select('pw_policy', where="userid=$id_user", order="date DESC", vars=locals())
             if len(check) > 0:
-                notfound=0
-              #  result_get = db.select('pw_policy', where="idpolicy=$id_tmp", vars=locals())[0]
                 result_get = check[0]
                 session.mysession.session.date = result_get.date
                 return render.pwpolicy_form(session.mysession.session.user,result_get.userid, result_get.plen, result_get.psets,
                                 result_get.pdict, result_get.phist, result_get.prenew,
-                                result_get.pattempts, result_get.pautorecover, notfound, str(result_get.date))
+                                result_get.pattempts, result_get.pautorecover, 0, str(result_get.date))
             else:
-                notfound=1
 #                dt = datetime.now()
 #                dtt = dt - timedelta(days=dt.weekday()) #goes back to last monday
                 # The default policy (i.e. when not specified by user)
@@ -45,7 +46,7 @@ class pwpolicy:
                 session.mysession.session.date = result_get.date
                 return render.pwpolicy_form(session.mysession.session.user, result_get.userid, result_get.plen, result_get.psets,
                                 result_get.pdict, result_get.phist, result_get.prenew,
-                                result_get.pattempts, result_get.pautorecover, notfound, result_get.date)
+                                result_get.pattempts, result_get.pautorecover, 1, result_get.date)
         else:
             raise web.seeother('/home')
 
