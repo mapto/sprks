d3e8524eadee42ebde365de35015933b7916d4c9
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2013-06-26 13:51:14 +0100

    Added incidents controller

---
+++ models/incident.py
@@ -0,0 +1,15 @@
+__author__ = 'zcabh_000'
+from sklearn import svm
+
+
+class incident:
+    classes = [range(1, 8)]
+
+    def get_incident_description(self, cls):
+        pass
+
+    def get_incident_risk(self, cls):
+        pass
+
+    def get_incident_cost(self, cls):
+        pass
