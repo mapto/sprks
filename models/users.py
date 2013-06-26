52294e06091459cd5e2286d161844ad4a65c9e73
Horace Li <me@horaceli.com>
2013-06-26 11:09:00 +0100

    Moved some shit around

--- models/users.py
+++ models/users.py
@@ -3,7 +3,6 @@ __author__ = 'Horace'
 import web
 from libraries.utils import hash_utils
 from environment import db
-from sim.simulation import simulation
 
 
 class users_model:
@@ -84,18 +83,3 @@ class users_model:
             return True
         else:
             return False
-
-    def get_policy_history(self, id):
-        history = db.select('pw_policy', where="userid=$id", order="date", vars=locals())
-        history_dict = []
-        sim = simulation()
-        for row in history:
-            tmp = {}
-            for k, v in row.iteritems():
-                tmp[k] = str(v)
-                if k != 'idpolicy' and k != 'userid' and k != 'date':
-                    sim.set_policy(k, v)
-            tmp['risk'] = sim.calc_risk_prob()
-            tmp['cost'] = sim.calc_prod_cost()
-            history_dict.append(tmp)
-        return history_dict
