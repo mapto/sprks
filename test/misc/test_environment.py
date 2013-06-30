95129d6531e85c76b5011bc7538a9fd430f7240e
Horace Li <horaceli@gmail.com>
2013-06-30 17:46:36 +0100

    Mother of all bug fixes

--- test/misc/test_environment.py
+++ test/misc/test_environment.py
@@ -12,8 +12,7 @@ class TestWorkEnv:
         assert os.getcwd() == os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
     def test_static_files(self):
-        assert os.path.isfile('static/css/bootstrap.css')
-        assert os.path.isfile('static/img/glyphicons-halflings.png')
+        assert os.path.isfile('static/img/!glyphicons-halflings.png')
         assert os.path.isfile('static/data/pw-train-data-full.csv')
         # include other static files that are required
 
