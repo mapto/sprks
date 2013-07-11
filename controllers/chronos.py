8fcb5737079879b9479134f1f72306e8db8bef80
Daniyar <kaliev.daniyar@gmail.com>
2013-07-11 16:48:58 +0100

    UPdated chronos controller

--- controllers/chronos.py
+++ controllers/chronos.py
@@ -31,6 +31,14 @@ class chronos:
 
         policy_update = payload.get('policyUpdate')
 
+        updated_policy = {}
+
+        if policy_update is not None:
+            updated_policy = policies_model().parse_policy()
+            latest_policy = policies_model().iter_to_nested_obj(policies_model().get_latest_policy(context.user_id()))
+            merged_policy = policies_model().merge_policies(updated_policy, latest_policy)
+            print policies_model().nested_obj_to_list_of_dict(merged_policy)
+
         if sync_date.day == 1 and policy_update is not None:
             for policy_change in policy_update.iteritems():
                 policy_change
