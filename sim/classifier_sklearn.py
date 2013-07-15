6abc44dea43b43bb3a4b8d1d1f3bd9aace3b8da5
Daniyar <kaliev.daniyar@gmail.com>
2013-07-15 15:29:04 +0100

    Updated method for returning risks list. Havent tested

--- sim/classifier_sklearn.py
+++ sim/classifier_sklearn.py
@@ -6,6 +6,7 @@ import glob
 from models.incident import incident
 import json
 from models.pw_policy import pw_policy_model as pw_policy
+from models.company import company
 
 
 class classifier_sklearn:
@@ -19,6 +20,7 @@ class classifier_sklearn:
         """
         self.incidents_models = {}
         self.risks = []
+        self.risks_set = ["bruteforce", "stolen"]
 
         """ Currently the data model in the simulation is dependent on pw_policy
             This should be generalized and made dependent on policies
@@ -27,7 +29,7 @@ class classifier_sklearn:
 
         general = numpy.genfromtxt('static/data/pw-train-generated-general.csv', delimiter=',')
 
-        for filename in glob.glob('static/data/pw-train-generated-risk-*.csv'):
+        """ for filename in glob.glob('static/data/pw-train-generated-risk-*.csv'):
             risk = filename[36:-4] # take actual name
             self.risks.append(risk)
             # data = genfromtxt('static/data/pw-train-estimator-risk-' + risk + '.csv', delimiter=',')
@@ -36,6 +38,21 @@ class classifier_sklearn:
             train_data = data[:, 0:limit] # first several columns represent the data dimension
             train_result = data[:, limit] # result columns are ones after data dimensions
             self.incidents_models[risk] = svm.SVC().fit(train_data, train_result)
+        """
+
+        for risk in self.risks_set:
+            if not risk in self.incidents_models.keys():
+                self.incidents_models[risk] = {}
+            for employee in company.employee_types:
+                if not employee in self.incidents_models[risk].keys():
+                    self.incidents_models[risk][employee] = {}
+                for location in company.location_types:
+                    if not location in self.incidents_models[risk][employee].keys():
+                        self.incidents_models[risk][employee][location] = {}
+                    for device in company.device_types:
+                        if not device in self.incidents_models[risk][employee][location].keys():
+                            self.incidents_models[risk][employee][location][device] = {}
+                        self.incidents_models[risk][employee][location][device] = self.generate_training_data(risk, employee, location, device, limit, general)
             # self.incidents_models[risk] = svm.SVR().fit(train_data, train_result)
 
         # print self.risks
@@ -59,22 +76,44 @@ class classifier_sklearn:
         greatest = None
 
         risks_list = []
+        tmp_list = {}
 
-        for risk in self.risks:
-            cls = self.incidents_models[risk].predict(datapoints)[0]
-            cls = int(cls) # algorithm returns float, we need to match it to string defined in json
-            # data is returned as an array of numpy.float64, we need integers so we could use them as incident indices
-            # event = incident(cls[0].astype(int64))
-            # risk = event.get_risk()
-            event = incident.get_incident(cls)
-
-            risks_list.append(event["id"])
-
+        #for risk in self.risks:
+        for risk in self.risks_set:
+            my_list = []
+            for employee in company.employee_types:
+                for location in company.location_types:
+                    for device in company.device_types:
+                        cls = self.incidents_models[risk][employee][location][device].predict(datapoints)[0]
+                        cls = int(cls) # algorithm returns float, we need to match it to string defined in json
+                        # data is returned as an array of numpy.float64, we need integers so we could use them as incident indices
+                        # event = incident(cls[0].astype(int64))
+                        # risk = event.get_risk()
+                        event = incident.get_incident(cls)
+                        my_list.append(event['id'])
+                        #risks_list.append(event["id"])
+            tmp_list[risk] = my_list
             #if greatest is None or event["risk"] > greatest[1]:
             #   greatest = [event["name"], event["risk"]] # 0 - name, 1 - risk
 
+        max = 1
+        for risk in tmp_list:
+            for value in tmp_list[risk]:
+                if value < max:
+                    max = value
+            risks_list.append(max)
+            max = 1
+
         return risks_list
         #return self.incidents_model.predict(data)
+
+    def generate_training_data(self, risk, employee, location, device, limit, general):
+        filename = glob.glob('static/data/pw-train-generated-'+risk+'-'+employee+location+device+'.csv')
+        data = numpy.genfromtxt(filename, delimiter=',')
+        data = numpy.concatenate((data, general)) # add positive cases that need to contrast negative ones
+        train_data = data[:, 0:limit] # first several columns represent the data dimension
+        train_result = data[:, limit] # result columns are ones after data dimensions
+        return svm.SVC().fit(train_data, train_result)
 
 
 if __name__ == "__main__":
