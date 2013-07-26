77152f182fa8291ef16c219f13eb4fe95a25971d
Zhanelya <subebo91@inbox.ru>
2013-07-26 11:26:38 +0100

    Updated interactive characters

--- controllers/characters.py
+++ controllers/characters.py
@@ -8,11 +8,16 @@ from models.company import company
 
 class characters:
     """
-    Listens to ajax call which updates characters' state
+    Types of characters:
+    employee_types = {'executives', 'desk', 'road'}
+
+    Listens to ajax call which updates characters' state:
+    location_types = {'office', 'public', 'home'}
+    device_types = {'desktop', 'laptop', 'phone'}
     """
     def POST(self):
         #takes date as an argument from ajax call and returns
-        #(TODO) corresponding to this moment of time
+        #corresponding to this moment of time
         # pair location-device for each of the 3 employees
         #considering the distributions from company.py
 
@@ -21,25 +26,34 @@ class characters:
         start_date = date_utils.iso8601_to_date('2014-01-06')
 
         days_delta = (client_date - start_date).days
-        location = []
 
+        locations, devices = self.get_locations_devices(days_delta)
+
+        locations_devices = json.dumps(
+            {
+                'interviewee1': [locations[0],devices[0]],
+                'interviewee2': [locations[1],devices[1]],
+                'interviewee3': [locations[2],devices[2]]
+            }
+        )
+        return locations_devices
+
+    def get_locations_devices(self, days_delta):
+        #TODO function deriving locations and devices array from the timedelta(current_client_time - start_time)
+        locations = []
+        devices = []
         if (days_delta % 10) <= 1:                      # mod <= 1
-            location = ['home', 'home', 'home']         # location [interviewee1, interviewee2, interviewee3]
+            locations = ['home', 'home', 'home']         # location [interviewee1, interviewee2, interviewee3]
+            devices = ['laptop', 'laptop', 'laptop']     # device [interviewee1, interviewee2, interviewee3]
         else:
             if (days_delta % 10) <= 6:                  # 1 < mod <= 6
-                location = ['office', 'office', 'office']
+                locations = ['office', 'office', 'office']
+                devices = ['desktop', 'desktop', 'desktop']
             else:                                       # mod > 6
-                location = ['public', 'public', 'public']
+                locations = ['public', 'public', 'public']
+                devices = ['phone', 'phone', 'phone']
 
         #may be used later for getting the appropriate time distribution (now being hardcoded):
         #location_distribution = company().get_location_distribution()
 
-
-        locations_devices = json.dumps(
-            {
-                'interviewee1': [location[0],'phone'],
-                'interviewee2': [location[1],'laptop'],
-                'interviewee3': [location[2],'desktop']
-            }
-        )
-        return locations_devices
+        return locations, devices
