6e1e0c117650fe98aabcbb1bf76173cf890c27fa
Zhanelya <subebo91@inbox.ru>
2013-07-26 14:13:26 +0100

    Finalised characters changing context

--- controllers/characters.py
+++ controllers/characters.py
@@ -1,7 +1,6 @@
 __author__ = 'Zhanelya'
 import json
 import web
-from datetime import date
 from libraries.utils import date_utils
 from models.company import company
 
@@ -22,7 +21,7 @@ class characters:
         #considering the distributions from company.py
 
         payload = json.loads(web.data())
-        client_date = date_utils.iso8601_to_date(payload.get('date', '2014-01-06'))
+        client_date = date_utils.iso8601_to_date(payload.get('date'))
         start_date = date_utils.iso8601_to_date('2014-01-06')
 
         days_delta = (client_date - start_date).days
@@ -31,29 +30,48 @@ class characters:
 
         locations_devices = json.dumps(
             {
-                'interviewee1': [locations[0],devices[0]],
-                'interviewee2': [locations[1],devices[1]],
-                'interviewee3': [locations[2],devices[2]]
+                'interviewee1': [locations[0],devices[0]],  # Susie (executive)
+                'interviewee2': [locations[1],devices[1]],  # Kevin (desk)
+                'interviewee3': [locations[2],devices[2]]   # Iza (road)
             }
         )
         return locations_devices
 
     def get_locations_devices(self, days_delta):
-        #TODO function deriving locations and devices array from the timedelta(current_client_time - start_time)
-        locations = []
-        devices = []
-        if (days_delta % 10) <= 1:                      # mod <= 1
-            locations = ['home', 'home', 'home']         # location [interviewee1, interviewee2, interviewee3]
-            devices = ['laptop', 'laptop', 'laptop']     # device [interviewee1, interviewee2, interviewee3]
-        else:
-            if (days_delta % 10) <= 6:                  # 1 < mod <= 6
-                locations = ['office', 'office', 'office']
-                devices = ['desktop', 'desktop', 'desktop']
-            else:                                       # mod > 6
-                locations = ['public', 'public', 'public']
-                devices = ['phone', 'phone', 'phone']
+        """
+        Function deriving locations and devices array
+        from the csv files considering timedelta(current_client_time - start_time)
+        #1 day is taken as a time unit -> 100days corresponds to 100%
+        """
 
-        #may be used later for getting the appropriate time distribution (now being hardcoded):
-        #location_distribution = company().get_location_distribution()
+        mod = (days_delta % 100)/float(100)
+        print days_delta
+        print mod
+        locations = [None] * 3
+        devices = [None] * 3
+
+        #locations for interviewees(1,2,3)
+        employees2locations = company().employees2locations
+        for i in range(0, 3):
+            if mod <= employees2locations[0, :][i]:    # percentage derived from locations.csv
+                locations[i] = 'home'
+            else:
+                if mod <= (employees2locations[0, :][i])+(employees2locations[1, :][i]):    # (percentage from locations.csv) + (previous one)
+                    locations[i] = 'public'
+                else:
+                    if mod > (employees2locations[0, :][i])+(employees2locations[1, :][i]):
+                        locations[i] = 'office'
+
+        #devices for interviewees(1,2,3)
+        employees2devices = company().employees2devices
+        for i in range(0, 3):
+            if mod <= employees2devices[0, :][i]:    # percentage derived from devices.csv
+                devices[i] = 'desktop'
+            else:
+                if mod <= (employees2devices[0, :][i])+(employees2devices[1, :][i]):    # (percentage from devices.csv) + (previous one)
+                    devices[i] = 'laptop'
+                else:
+                    if mod > (employees2devices[0, :][i])+(employees2devices[1, :][i]):
+                        devices[i] = 'phone'
 
         return locations, devices
