75b85223624ad0c7c61774ccff939b9098934e06
Zhanelya <subebo91@inbox.ru>
2013-07-30 16:07:20 +0100

    Fixed profile page request

--- static/js/profile.js
+++ static/js/profile.js
@@ -11,86 +11,7 @@ var graph_data;
 function initProfile() {
 
     get_profile(); //history values in json format from serverside (policy_history.py) by ajax call written to policy_history var
-    console.log(json);
-    console.log(policy_history);
-    console.log(graph_data);
 
-    createGraph(graph_data);
-
-    //create table dynamically:
-    var table = $('<table></table>').addClass('profile_table');
-
-    //provide column names:
-    var row = $('<tr></tr>').addClass('profileTr');
-    var date = $('<td></td>').addClass('profileTd_date profileTh').text("date");
-    row.append(date);
-    for (var j in policy_history[0]) {
-        var attrName = j; //e.g. pdict
-        if (attrName !== 'date' && attrName !== 'id_policy'  && attrName !== 'id'  && attrName !== 'pw_id'  && attrName !== 'bio_id' && attrName !== 'pass_id' && attrName !== 'user_id' && attrName !== 'cost' && attrName !== 'risk') { //do not show these fields
-            var col = $('<td></td>').addClass('profileTd profileTh').text(attrName);
-            row.append(col);
-        }
-    }
-    table.append(row);
-
-    //fill table:
-    var prev_obj = '';
-    var row = [];
-    var date=[];
-    var col = [];
-    var tmp;
-
-    for (var i in policy_history) {
-        var obj = policy_history[i];
-        col[i] = {};
-        row [i] = $('<tr></tr>').addClass('profileTr'+i);
-        date [i] = $('<td></td>').addClass('profileTd_date').text(time_visualiser(obj['date'], false));
-        row [i].append(date[i]);
-        for (var k in obj) {
-            var attrName = k; //e.g. pdict
-            var attrValue = obj[k]; //e.g. 1
-            if (attrName !== 'date' && attrName !== 'id_policy' && attrName !== 'id'  && attrName !== 'pw_id'  && attrName !== 'bio_id' && attrName !== 'pass_id' && attrName !== 'user_id' && attrName !== 'cost' && attrName !== 'risk') { //do not show these fields
-                if (i < 1) { //if it's first row
-                    col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text(attrValue);
-                    row [i].append(col[i][attrName]);//add all policy values
-                } else if (i > 0 && (obj[k] !== prev_obj[k])) { //if it's second row
-                    //if (attrName !== 'employee' && attrName !== 'location' && attrName !== 'device' ){
-                    //    col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text('changed from ' + prev_obj[k] + ' to ' + obj[k]);
-                    //}else{
-                        col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text(obj[k]);
-                    //}
-                    row [i].append(col[i][attrName]); //add value column only if value have changed
-                } else {
-                    col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text('');
-                    row [i].append(col[i][attrName]); //add empty column if no changes
-                }
-            }
-        }
-
-        if(row[i].text()!=''){ //check if the row contains anything
-            if(row[i].text().substring(10).match(/\d+/g)){ //exclude date, check if the rest row contains number values( such as for plen, etc.)
-                var empty_row = $('<tr></tr>').addClass('profileTr profileTh');
-                for (var x=0; x<13;x++){
-                    empty_row.append($('<td></td>').addClass('profileTd profileTh').text(''));
-                }
-
-                table.append(empty_row);
-
-                //table.append(row[i]);
-                //tmp=i;
-            }else{                                      //if row contains only environmental variables (location/employee/device
-
-                row[i]
-
-
-
-            }
-        table.append(row[i]);
-        }
-        prev_obj = policy_history[i];
-    }
-
-    $('#profile_table').append(table);
 }
 
 
@@ -103,6 +24,71 @@ function get_profile(){
             json = JSON.parse(data);
             policy_history = JSON.parse(json['policy_history']);
             graph_data = JSON.parse(json['graph_data']);
+
+            createGraph(graph_data);
+
+            //create table dynamically:
+            var table = $('<table></table>').addClass('profile_table');
+
+            //provide column names:
+            var row = $('<tr></tr>').addClass('profileTr');
+            var date = $('<td></td>').addClass('profileTd_date profileTh').text("date");
+            row.append(date);
+            for (var j in policy_history[0]) {
+                var attrName = j; //e.g. pdict
+                if (attrName !== 'date' && attrName !== 'id_policy'  && attrName !== 'id'  && attrName !== 'pw_id'  && attrName !== 'bio_id' && attrName !== 'pass_id' && attrName !== 'user_id' && attrName !== 'cost' && attrName !== 'risk') { //do not show these fields
+                    var col = $('<td></td>').addClass('profileTd profileTh').text(attrName);
+                    row.append(col);
+                }
+            }
+            table.append(row);
+            //fill table:
+            var prev_obj = '';
+            var row = [];
+            var date=[];
+            var col = [];
+            var tmp;
+            for (var i in policy_history) {
+                var obj = policy_history[i];
+                col[i] = {};
+                row [i] = $('<tr></tr>').addClass('profileTr'+i);
+                date [i] = $('<td></td>').addClass('profileTd_date').text(time_visualiser(obj['date'], false));
+                row [i].append(date[i]);
+                for (var k in obj) {
+                    var attrName = k; //e.g. pdict
+                    var attrValue = obj[k]; //e.g. 1
+                    if (attrName !== 'date' && attrName !== 'id_policy' && attrName !== 'id'  && attrName !== 'pw_id'  && attrName !== 'bio_id' && attrName !== 'pass_id' && attrName !== 'user_id' && attrName !== 'cost' && attrName !== 'risk') { //do not show these fields
+                        if (i < 1) { //if it's first row
+                            col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text(attrValue);
+                            row [i].append(col[i][attrName]);//add all policy values
+                        } else if (i > 0 && (obj[k] !== prev_obj[k])) { //if it's second row
+                            //if (attrName !== 'employee' && attrName !== 'location' && attrName !== 'device' ){
+                            //    col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text('changed from ' + prev_obj[k] + ' to ' + obj[k]);
+                            //}else{
+                                col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text(obj[k]);
+                            //}
+                            row [i].append(col[i][attrName]); //add value column only if value have changed
+                        } else {
+                            col[i][attrName] = $('<td></td>').addClass('profileTd '+attrName+i).text('');
+                            row [i].append(col[i][attrName]); //add empty column if no changes
+                        }
+                    }
+                }
+                if(row[i].text()!=''){ //check if the row contains anything
+                    if(row[i].text().substring(10).match(/\d+/g)){ //exclude date, check if the rest row contains number values( such as for plen, etc.)
+                        var empty_row = $('<tr></tr>').addClass('profileTr profileTh');
+                        for (var x=0; x<13;x++){
+                            empty_row.append($('<td></td>').addClass('profileTd profileTh').text(''));
+                        }
+                        table.append(empty_row);
+                    }else{                                      //if row contains only environmental variables (location/employee/device
+                        row[i]
+                    }
+                table.append(row[i]);
+                }
+                prev_obj = policy_history[i];
+            }
+            $('#profile_table').append(table);
         },
         error: function(response) {
             console.log("fail: " + response.responseText);
