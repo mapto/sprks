4c3bdaa1c2711712edc0f78af603fd327053348c
Horace Li <me@horaceli.com>
2013-07-31 11:25:36 +0100

    Cleaning up, moved knockout bindings to respective js files

--- static/js/statusbar.js
+++ static/js/statusbar.js
@@ -1,26 +1,36 @@
 /*
-Before any AJAX call, run statusUpdating().
-In all AJAX callbacks, run statusReady().
+ Before any AJAX call, run statusUpdating().
+ In all AJAX callbacks, run statusReady().
  */
 
-function statusUpdating(){
+statusBarModel = {
+    statusbar_status: ko.observable(),
+    statusbar_image: ko.observable()
+};
+
+function statusUpdating() {
     statusBarModel.statusbar_image('static/img/ajax-loader.gif');
     statusBarModel.statusbar_status('Synchronising with server...')
 }
 
-function statusReady(){
+function statusReady() {
     statusBarModel.statusbar_image('static/img/check.png');
     statusBarModel.statusbar_status('Synchronization complete. Ready.')
 }
 
-$(function(){
+$(function () {
+
+
+    ko.applyBindings(statusBarModel, document.getElementById('statusBar'));
+
     statusBarModel.statusbar_image('static/img/check.png')
     statusBarModel.statusbar_status('Ready.')
 })
 
-function manage_toast_alert(text, delay){
+function manage_toast_alert(text, delay) {
     $("#toast").text(text);
     $("#toast").show();
     $('#toast').delay(delay).fadeOut();
 
 }
+
