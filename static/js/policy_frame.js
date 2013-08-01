4856e911e41f01765d4b770fd3ab82ac53f340e3
Zhanelya <subebo91@inbox.ru>
2013-08-01 11:31:16 +0100

    Updated pooilcy_frame

--- static/js/policy_frame.js
+++ static/js/policy_frame.js
@@ -8,6 +8,11 @@
 
 //summary of the policy (right corner), called on any policy change
 function summarize_policy(policy){
+    console.log(policy.employee);
+    console.log(policy.location);
+    console.log(policy.device);
+    console.log(policy.policyDelta);
+    console.log('end');
     for (var key in policy){
         if(key==='employee'||key==='location'||key==='device'){
             $("#sum-"+key).text(policy[key]);
@@ -16,11 +21,27 @@ function summarize_policy(policy){
                 var plc = [];
                 for (var j in policy[key][k]){
                     if (j==='plen'){
-                        if (policy[key][k][j]==='0'){plc = []; break;} //null password policy in summary if plen is set to '0'
+                        if (policy[key][k][j]==='0'){plc.push('policy disabled'); break;}  //let user know that policy is disabled if value of password length is 0
+                    }
+                    if (j==='pdata'){
+                        if (policy[key][k][j]==='0'){plc.push('policy disabled'); break;}  //let user know that policy is disabled if value of bio data is 0
+                    }
+                    if (j==='bdata'){
+                        if (policy[key][k][j]==='0'){plc.push('policy disabled'); break;}  //let user know that policy is disabled if value of pass data is 0
                     }
                     plc.push(j+' '+policy[key][k][j]);
                 }
                 $("#sum-"+k).text(plc);
+                $('.sum-label').each(function(){                            //iterate through policies labels
+                    var sumlabel = $(this).attr('class').replace('sum-label', '').replace(' ','').replace('sum-','') ; //biometric/passfaces/pwpolicy
+                    if(sumlabel==k){
+                        if(!(Object.keys(policy.policyDelta[k]).length===0)){
+                           $(this).show();                                              //show policy label if it's not empty'
+                        }else{
+                           $(this).hide();                                              //hide otherwise
+                        }
+                    }
+                });
             }
         }
         $('.policy-summary').show();
