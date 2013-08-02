c628e566be55ffe96a56b1cbeeb1841b2cab03cf
Zhanelya <subebo91@inbox.ru>
2013-08-02 16:54:54 +0100

    Updated profile page table

--- static/js/policy.js
+++ static/js/policy.js
@@ -264,7 +264,7 @@ $("#apply").click(function () {
         toastr['info']('Policy saved. All the changes will be applied in the end of the term. Once you have finished updating the policies, please press the play button to continue');
     }
 
-    submitAlternativesRequest();
+    //submitAlternativesRequest();
 
 });
 
